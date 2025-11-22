763030fb9e0ceba5e0ef8d0e4ea2bbe7c72d1515
Kleibert Medina <90476700+KleibertM@users.noreply.github.com>
2025-11-22 09:16:52 -0500

    Add search functionality to Cards component

--- client/src/component/Cards/Cards.jsx
+++ client/src/component/Cards/Cards.jsx
@@ -1,16 +1,27 @@
 import CardComponent from '../Card/Card'
-// import { data } from '../Data/data.json'
 import { dataMunay } from '../Data/data'
 import { Grid, GridItem } from '@chakra-ui/react'
+import Searcher from '../Searcher'
+import { useState } from 'react'
+
 const CardsContent = () => {
+    const [search, setSearch] = useState('')
+
+    const searcher = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const results = !search ? dataMunay : dataMunay.filter((dataMunay) => dataMunay.nombre.toLowerCase().includes(search.toLowerCase()));
     return (
         <>
+            <Searcher searcher={searcher} />
             <Grid h='auto' w={'100vw'}
                 templateRows={['repeat(1, 1fr)', 'repeat(2, 1fr)']}
-                templateColumns={['repeat(2, 1fr)','repeat(2, 1fr)',   'repeat(4, 1fr)']}
-                gap={[4, 8]} position={'relative'} 
-                >
-                {dataMunay.map((item, index) => (
+                templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']}
+                gap={[4, 8]} position={'relative'}
+            >
+
+                {results.map((item, index) => (
                     <>
                         <GridItem key={index} margin={'0 auto'} >
                             <CardComponent dataItem={item} />
